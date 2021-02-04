19e287038599d6c576290619f09e6460237e5b4b
Andres V <andres@glitchy.website>
2021-02-04 09:34:36 +0100

    erste commit

---
+++ src/index.js
@@ -0,0 +1,245 @@
+import "7.css/dist/7.css";
+import "./styles.css";
+
+import interact from "interactjs";
+
+const winTemplate = `
+<div class="window-wrapper">
+  <div class="window colored glass">
+    <div class="title-bar dbl-maximize">
+      <div class="title-bar-text">{title}</div>
+      <div class="title-bar-controls">
+        <button aria-label="Minimize"></button>
+        <button aria-label="Maximize" class="maximize"></button>
+        <button aria-label="Close" class="close"></button>
+      </div>
+    </div>
+    <div class="window-body">
+    </div>
+  </div>
+</div>
+`;
+
+function htmlToElement(html) {
+  const template = document.createElement("template");
+  html = html.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+function getElementOffset(el) {
+  let top = 0;
+  let left = 0;
+  let element = el;
+
+  do {
+    top += element.offsetTop || 0;
+    left += element.offsetLeft || 0;
+    element = element.offsetParent;
+  } while (element);
+
+  return {
+    top,
+    left
+  };
+}
+
+function snap(win, snapEl) {
+  snapEl.style.opacity = 0;
+  win.element.classList.add("snapping");
+  setTimeout(() => {
+    win.element.classList.remove("snapping");
+  }, 400);
+
+  if (win.state.status === "maximized" && snapEl === dropFull) {
+    win.saveState({
+      ...win.prevState,
+      snapped: false,
+      snapTo: "",
+      status: ""
+    });
+  } else {
+    let { top: y, left: x } = getElementOffset(snapEl);
+    let w = snapEl.offsetWidth;
+    let h = snapEl.offsetHeight;
+    win.saveState({
+      w,
+      h,
+      x,
+      y,
+      snapped: true,
+      snapTo: "",
+      status: snapEl === dropFull ? "maximized" : ""
+    });
+  }
+}
+
+let zIndex = 1;
+let windowId;
+const container = document.querySelector("#container");
+const dropLeft = document.querySelector("#drop-left");
+const dropRight = document.querySelector("#drop-right");
+const dropFull = document.querySelector("#drop-full");
+const windows = [];
+
+function makeWindow(opts) {
+  const { title = "", content = null } = opts;
+  const el = htmlToElement(winTemplate.replace("{title}", title));
+  const id = windowId++;
+
+  let state = {};
+  let prevState = {};
+  function saveState(newState) {
+    prevState = { ...state };
+    state = {
+      ...state,
+      ...newState
+    };
+
+    Object.assign(el.style, {
+      width: `${state.w}px`,
+      height: `${state.h}px`,
+      top: `${state.y}px`,
+      left: `${state.x}px`
+    });
+  }
+
+  const win = {
+    element: el,
+    get prevState() {
+      return prevState;
+    },
+    get state() {
+      return state;
+    },
+    saveState
+  };
+
+  el.querySelector(".window-body").appendChild(content);
+  container.appendChild(el);
+
+  el.querySelector(".maximize").addEventListener("click", function () {
+    snap(win, dropFull);
+  });
+
+  el.querySelector(".dbl-maximize").addEventListener("dblclick", function () {
+    snap(win, dropFull);
+  });
+
+  el.querySelector(".close").addEventListener("click", function () {
+    container.removeChild(el);
+    delete windows[id];
+  });
+
+  const w = el.offsetWidth;
+  const h = el.offsetHeight;
+  const x = (container.offsetWidth - el.offsetWidth) / 2;
+  const y = (container.offsetHeight - el.offsetHeight) / 2;
+
+  win.saveState({ x, y, w, h, snapTo: "", snapped: false, status: "" });
+
+  el.addEventListener("mousedown", function () {
+    Object.assign(el.style, { zIndex: zIndex++ });
+  });
+
+  interact(el)
+    .resizable({
+      margin: 7,
+      edges: { left: true, right: true, bottom: true, top: true },
+      listeners: {
+        move(event) {
+          let { x, y } = win.state;
+          x += event.deltaRect.left;
+          y += event.deltaRect.top;
+          const w = event.rect.width;
+          const h = event.rect.height;
+
+          win.saveState({ x, y, w, h });
+        }
+      },
+      modifiers: [
+        // keep the edges inside the parent
+        interact.modifiers.restrictEdges({
+          outer: "parent"
+        }),
+
+        // minimum size
+        interact.modifiers.restrictSize({
+          min: { width: win.state.w, height: win.state.h }
+        })
+      ],
+      inertia: true
+    })
+    .draggable({
+      allowFrom: ".title-bar",
+      ignoreFrom: "button",
+      listeners: {
+        move: function dragMoveListener(event) {
+          let { x, y, w, h, snapTo, snapped } = win.state;
+
+          if (snapped) {
+            w = win.prevState.w;
+            h = win.prevState.h;
+            x = event.pageX - event.pageX / (win.state.w / w) + x / 2;
+            y = event.pageY;
+            snapped = false;
+          } else {
+            x += event.dx;
+            y += event.dy;
+          }
+
+          if (event.pageX <= 0) {
+            dropLeft.style.opacity = 1;
+            snapTo = "left";
+          } else if (event.pageX >= container.offsetWidth) {
+            dropRight.style.opacity = 1;
+            snapTo = "right";
+          } else if (event.pageY <= 0) {
+            dropFull.style.opacity = 1;
+            snapTo = "top";
+          } else {
+            if (event.buttons > 0) {
+              snapTo = "";
+            }
+            dropLeft.style.opacity = 0;
+            dropRight.style.opacity = 0;
+            dropFull.style.opacity = 0;
+          }
+          win.saveState({ x, y, w, h, snapTo, snapped, status: "" });
+        },
+        end: function () {
+          let { snapTo } = win.state;
+          let snapEl;
+          if (snapTo === "left") {
+            snapEl = dropLeft;
+          }
+          if (snapTo === "right") {
+            snapEl = dropRight;
+          }
+          if (snapTo === "top") {
+            snapEl = dropFull;
+          }
+          if (snapEl) {
+            snap(win, snapEl);
+          }
+        }
+      }
+    });
+
+  windows[windowId] = win;
+
+  return win;
+}
+
+for (let i = 0; i < 2; i++) {
+  makeWindow({
+    title: "Hello",
+    content: htmlToElement(`<div><p>Hello</p>
+    <section class="field-row" style="justify-content: flex-end;">
+      <button>OK</button>
+      <button>Cancel</button>
+    </section></div>`)
+  });
+}
+
+// interact(target).unset()
